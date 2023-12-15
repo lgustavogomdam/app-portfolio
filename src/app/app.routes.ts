e343e241787c74cb5d30f76d913171afb8225c5b
lgustavogomdam <gustavo.gomdam@gmail.com>
2023-12-14 21:37:48 -0300

    Alterando local do template

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,18 @@
 import { Routes } from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
 
   {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
     path: '',
     pathMatch: 'full',
-    component: AppComponent
+    redirectTo: '/home'
   },
   {
     path: '**',
