293188a9465e4242752be1d5e102b85ef18e6b07
lgustavogomdam <gustavo.gomdam@gmail.com>
2023-12-15 00:11:37 -0300

    Corrigindo roteamento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,14 @@
 import { Routes } from '@angular/router';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { AppComponent } from './app.component';
+
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes = [
 
   {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
     path: '',
     pathMatch: 'full',
-    redirectTo: '/home'
+    component: HomeComponent
   },
   {
     path: '**',
